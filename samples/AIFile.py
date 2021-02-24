ad5552a5c4c8b6eac3507aeeeeb2e338654f24d8
Alberto López Sánchez <alopez@cells.es>
2021-02-24 17:04:50 +0100

    Fix sample to work

--- samples/AIFile.py
+++ samples/AIFile.py
@@ -1,16 +1,16 @@
-from adlink import D200X
-import adlink
+from d2kdask import D200X
+import d2kdask
 
 
 def main():
 
     largest_channel = 3
-    v_range = adlink.VoltageRange.AD_B_10_V
+    v_range = d2kdask.VoltageRange.AD_B_10_V.value
     read_count = 4000
     BASETIME = 40000000
     SCAN_INTERVAL = 160
     sample_rate = BASETIME / SCAN_INTERVAL
-    file_name = "2005d.dat"
+    file_name = b"2005d"
 
     print(
         "This program inputs {} data from CH-0 to CH-{} of DAQ-2005/2206 in {} Hz".format(
@@ -24,11 +24,11 @@ def main():
     card_num = int(input("Please input a card number: "))
 
     if card_type == 0:
-        card_type = adlink.Card.DAQ_2005.value
+        card_type = d2kdask.Card.DAQ_2005.value
     elif card_type == 1:
-        card_type = adlink.Card.DAQ_2006.value
+        card_type = d2kdask.Card.DAQ_2006.value
     elif card_type == 2:
-        card_type = adlink.Card.DAQ_2016.value
+        card_type = d2kdask.Card.DAQ_2016.value
     else:
         print("Wrong card number")
         return -1
@@ -38,14 +38,14 @@ def main():
     for i in range(4):
         card.ai_channel_config(i, v_range)
 
-    card.ai_config(config_control=adlink.DAQ2K_AI_ADCONVSRC_Int,
-                   trigger_control=adlink.DAQ2K_AI_TRGSRC_SOFT,
+    card.ai_config(config_control=0,
+                   trigger_control=0,
                    middle_or_delay_scans=0,
                    m_counter=0,
                    re_trigger_counter=0,
                    auto_reset_buf=True)
 
-    card.ai_continous_scan_to_file(
+    card.ai_continuous_scan_to_file(
         filename=file_name, largest_channel=largest_channel,
         read_scans=read_count / (largest_channel + 1),
         scan_interval=SCAN_INTERVAL, sample_interval=SCAN_INTERVAL)
