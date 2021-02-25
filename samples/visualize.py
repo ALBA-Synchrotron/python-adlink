06e7b83e36a884cbfee1ca666739f5f553a6379a
Alberto López Sánchez <alopez@cells.es>
2021-02-25 17:48:42 +0100

    plots all the channels

--- samples/visualize.py
+++ samples/visualize.py
@@ -2,5 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 a = np.fromfile('2005d.dat', dtype=np.uint16, offset=68)
 plt.plot(a[10:][::4])
-plt.savefig("sinus.png")
-plt.show()
+plt.savefig("ch1.png")
+plt.clf()
+
+plt.plot(a[1:][::4])
+plt.savefig("ch2.png")
+plt.clf()
+
+plt.plot(a[2:][::4])
+plt.savefig("ch3.png")
+plt.clf()
+
+plt.plot(a[3:][::4])
+plt.savefig("ch4.png")
